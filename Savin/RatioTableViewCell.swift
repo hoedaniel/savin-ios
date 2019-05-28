fb9e73da993473aab8aae3819f4330e17e8765d8
Guildy Harvey <harvey.guildy@yahoo.co.id>
2019-05-28 11:08:42 +0700

    nambah Ratio

---
+++ Savin/RatioTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RatioTableViewCell.swift
+//  Savin
+//
+//  Created by Guildy Harvey on 28/05/19.
+//  Copyright © 2019 Guildy Harvey. All rights reserved.
+//
+
+import UIKit
+
+class RatioTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Savin/RatioViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RatioViewController.swift
+//  Savin
+//
+//  Created by Guildy Harvey on 28/05/19.
+//  Copyright © 2019 Guildy Harvey. All rights reserved.
+//
+
+import UIKit
+
+class RatioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
